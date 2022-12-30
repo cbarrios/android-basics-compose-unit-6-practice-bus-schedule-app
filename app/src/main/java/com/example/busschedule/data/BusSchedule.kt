0ad0cf38caff0bca3a93edf597d847df590ce952
cbarrios <lalobarrios16@yahoo.com>
2022-12-29 20:02:11 -0500

    master: (4) Create a Room entity

--- app/src/main/java/com/example/busschedule/data/BusSchedule.kt
+++ app/src/main/java/com/example/busschedule/data/BusSchedule.kt
@@ -16,8 +16,17 @@
 
 package com.example.busschedule.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "schedule")
 data class BusSchedule(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int = 0,
+    @ColumnInfo(name = "stop_name")
     val stopName: String,
+    @ColumnInfo(name = "arrival_time")
     val arrivalTimeInMillis: Int
 )
